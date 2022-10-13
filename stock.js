b517fb87b5adf8834b850c419ebdff1e31d37127
Alan Ezequiel Vargas <alanv3533@gmail.com>
2022-10-13 18:30:44 -0300

    first commit

---
+++ stock.js
@@ -0,0 +1,23 @@
+let listaProductos = [
+  {
+    id: 1,
+    nombre: "Playstation 5",
+    cantidad: 1,
+    precio: 125000,
+    img: "./assets/playstation5.jpg",
+  },
+  {
+    id: 2,
+    nombre: "Dragonball Z Kakarot",
+    cantidad: 1,
+    precio: 8000,
+    img: "./assets/dragon-ball-z-kakarot-standard-edition-for-xbox.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Razer Kraken",
+    cantidad: 1,
+    precio: 12000,
+    img: "./assets/razer.png",
+  }
+];
